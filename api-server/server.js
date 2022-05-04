4b59f88e647b3bafd098b97542039bbb5069f20f
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-04 13:56:44 -0600

    Add user and post routers to Express server

--- api-server/server.js
+++ api-server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const server = express();
 const cors = require('cors');
+const posts = require('./routes/posts');
+const users = require('./routes/users');
 const PORT = 3001;
 
 server.use(express.json());
 server.use(cors({
   origin: '*'
 }));
+
+server.use('/posts', posts);
+server.use('/users', users);
 
 server.get('/', (request, response) => {
   response.status(200).send("The server is functioning properly.");
