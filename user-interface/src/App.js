dc14743847d3dfef0ad54fba619170fc0adb24fd
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-05 12:53:30 -0600

    Provide context to child components to set current user

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
+import { createContext, useState } from 'react';
 import { Login, Logout } from './Utilities'
 
+const UserContext = createContext();
+
 function App() {
+  const [user, setUser] = useState({});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Login />
-        <Logout />
-        <p>
-          Placeholder content.
-        </p>
-      </header>
-    </div>
+    <UserContext.Provider value={{user, setUser}}>
+      <div className="App">
+        <header className="App-header">
+          <Login />
+          <Logout />
+          <p>
+            Placeholder content.
+          </p>
+        </header>
+      </div>
+    </UserContext.Provider>
   );
 }
 
+export { UserContext };
 export default App;
