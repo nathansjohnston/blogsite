129a1e99779998425fdbfeae21ad75aa60e0d09c
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-05 12:52:37 -0600

    Update users router to operate off of a username

--- api-server/routes/users.js
+++ api-server/routes/users.js
@@ -3,35 +3,45 @@ const router = express.Router();
 const knex = require('knex')(require('../knexfile.js')['development']);
 
 router
+  .get('/', (request, response) => {
+    knex.select('*').from('users')
+    .where('username', '=', request.query.username)
+    .andWhere('secret', '=', request.query.token)
+    .then(data => response.status(200).json(data[0]))
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+  })
   .get('/:id', (request, response) => {
     knex.select('*').from('users').where('id', '=', request.params.id)
       .then(data => response.status(200).json(data[0]));
   })
   .post('/', (request, response) => {
-    knex.insert(request.body).into('users')
+    knex.insert(request.body).into('users').onConflict('username').merge()
       .then(data => response.status(201).send("User account successfully created."))
       .catch(err => {
         console.log(err);
         throw err;
       });
   })
-  .patch('/:id', (request, response) => {
-    knex('users').where('id', '=', request.params.id).update(request.body)
+  .patch('/:username', (request, response) => {
+    knex('users').where('username', '=', request.params.username).update(request.body)
       .then(data => response.status(201).send("User account successfully updated."))
       .catch(err => {
         console.log(err);
         throw err;
       });
   })
-  .delete('/:id', (request, response) => {
-    knex('users').where('id', '=', request.params.id).delete()
+  .delete('/:username', (request, response) => {
+    knex('users').where('username', '=', request.params.username).delete()
       .then(data => response.status(200).send("User account successfully deleted."))
       .catch(err => {
         console.log(err);
         throw err;
       });
   })
-  .get('/:id/posts', (request, response) => {
+  .get('/:username/posts', (request, response) => {
     knex.select('*').from('posts').where('author_id', '=', request.params.id)
       .then(data => response.status(200).json(data))
       .catch(err => {
