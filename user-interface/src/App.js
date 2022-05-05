64817c4d9b624b7ccaf668d4911e6acbbee842e9
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-05 10:41:34 -0600

    Add GAPI components

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Login, Logout } from './Utilities'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Login />
+        <Logout />
         <p>
           Placeholder content.
         </p>
