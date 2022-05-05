6532e5e376b4617a7e47c41cfc7d78aad84ef682
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-05 15:29:27 -0600

    Use React Router to show non-NavBar content

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { createContext, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
+import { MyPosts } from './PostComponents';
 
 const UserContext = createContext();
 
@@ -11,6 +13,11 @@ function App() {
     <UserContext.Provider value={{user, setUser}}>
       <div className="App">
         <NavBar />
+        <Router>
+          <Routes>
+            <Route path='/' element={<MyPosts />}/>
+          </Routes>
+        </Router>
       </div>
     </UserContext.Provider>
   );
