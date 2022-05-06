4c295d1d5985429d3e00ce7092bdfd107417b12b
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-06 10:27:39 -0600

    Add Navigation buttons to the navigation bar

--- user-interface/src/NavBar.js
+++ user-interface/src/NavBar.js
@@ -1,14 +1,22 @@
 import './NavBar.css';
 import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { UserContext } from './App';
 import { Login, Logout } from './Utilities';
 
 function NavBar () {
   const userContext = useContext(UserContext);
+  const navigateTo = useNavigate();
   if (userContext.user.username) {
     return (
       <div className='NavBar'>
         <h3>{`Welcome, ${userContext.user.first_name} ${userContext.user.last_name}!`}</h3>
+        <button className='NavButton' onClick={() => {
+          navigateTo('/');
+        }}>All Posts</button>
+        <button className='NavButton' onClick={() => {
+          navigateTo('/profile');
+        }}>My Posts</button>
         <div className='NavLinks'>
           <Logout />
         </div>
@@ -17,6 +25,9 @@ function NavBar () {
   } else {
     return (
       <div className='NavBar'>
+        <button className='NavButton' onClick={() => {
+          navigateTo('/');
+        }}>All Posts</button>
         <div className='NavLinks'>
           <Login />
         </div>
