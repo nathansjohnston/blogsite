6ef0a074470f17bddccb9a82abe42a032340dcaa
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-06 08:48:58 -0600

    Add component to display all posts from all users

--- user-interface/src/PostComponents.js
+++ user-interface/src/PostComponents.js
@@ -4,6 +4,14 @@ import { UserContext } from './App';
 
 const API_ADDRESS = 'http://localhost:3001';
 
+function postContentHandler (postContent) {
+  if (postContent.length > 100) {
+    return postContent.slice(0, 100) + '...';
+  } else {
+    return postContent;
+  }
+}
+
 function MyPosts () {
   const userContext = useContext(UserContext);
   const [myPosts, setMyPosts] = useState([]);
@@ -27,12 +35,38 @@ function MyPosts () {
               <h5>{post.title}</h5>
               <h6>Posted on: {post.creation_date.slice(0, post.creation_date.indexOf('T'))}</h6>
             </header>
-            <p>{post.content}</p>
+            <p>{postContentHandler(post.content)}</p>
           </div>
         );
       })}
     </div>
   );
-}
+};
 
-export { MyPosts };
+function AllPosts () {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(API_ADDRESS + `/posts`)
+        .then(reply => reply.json())
+        .then(data => setPosts(data));
+  }, [])
+
+  return (
+    <div className='PostContainer'>
+      {posts.map(post => {
+        return (
+          <div key={post.id} className='PostIndividual'>
+            <header className='PostTitle'>
+              <h5>{post.title}</h5>
+              <h6>Posted on: {post.creation_date.slice(0, post.creation_date.indexOf('T'))}</h6>
+            </header>
+            <p>{postContentHandler(post.content)}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export { MyPosts, AllPosts };
