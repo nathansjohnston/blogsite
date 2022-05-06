29c06c6aab4741bde4959b9e5646884251e9f4cc
Nathaniel Johnston <j.johnston.tech@gmail.com>
2022-05-06 09:49:49 -0600

    Add state and route for single posts

--- user-interface/src/App.js
+++ user-interface/src/App.js
@@ -2,21 +2,23 @@ import './App.css';
 import { createContext, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import NavBar from './NavBar';
-import { MyPosts, AllPosts } from './PostComponents';
+import { MyPosts, AllPosts, Post } from './PostComponents';
 
 const UserContext = createContext();
 
 function App() {
   const [user, setUser] = useState({});
+  const [post, setPost] = useState(0);
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{user, setUser, post, setPost}}>
       <div className="App">
         <Router>
           <NavBar />
           <Routes>
             <Route path='/profile' element={<MyPosts />} />
             <Route path='/' element={<AllPosts />} />
+            <Route path='/post/:id' element={<Post />} />
           </Routes>
         </Router>
         <Outlet />
